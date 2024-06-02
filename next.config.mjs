e42320f5ff3d19e5872f81c2c5724e01cd07efbe
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-02 21:54:52 +0200

    Fix: Add Missing Hero (closes #6)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "8xsca0xnzcvircvx.public.blob.vercel-storage.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

d5d820db1b3c06e077ab8653b52b18b88b662d5b
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-11 20:33:28 +0200

    Update Header component styling
    - Removed px-3 and md:px-6 classes from header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="container flex items-center justify-between px-3 py-6 md:px-6">
+    <header className="container flex items-center justify-between py-6">
       <Link href="/" className="flex flex-col leading-none">
         <span className="font-bold uppercase">mads.codes</span>
         <span className="text-xs capitalize opacity-50">webudvikler</span>

630cfafe09525de4aabcbadaeffae77d29f636c0
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-21 15:42:55 +0200

    Update grid layout in Projects component
    - Change the CSS classes in the Projects component to incorporate different grid layouts for smaller and larger screens
    - Maintain export status of Projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ async function Projects() {
   const { rows } = await DAL.query.getProjects();
 
   return (
-    <section className="container col-span-full">
+    <section className="container col-span-full grid grid-cols-1 gap-48 md:grid-cols-2 md:gap-16">
       {rows.map((project) => (
         <Project key={project.id} project={project}></Project>
       ))}

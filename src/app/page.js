9b27decf6c8788eaa08af2c22091927034b174f4
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-26 22:10:12 +0200

    Add Terminal import and enhance text styling in Home component
    - Imported Terminal from "react-feather"
    - Enhanced text styling in the Home component:
      - Adjusted div and h1 styling, added flex and relative positioning
      - Updated text size from 5xl to 6xl
      - Added icon before "Hej der!"
      - Updated paragraph text styling, added tracking-tight property

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Story from "@/components/Story";
 
 import Projects from "@/components/Projects/Projects";
+import { Terminal } from "react-feather";
 
 function Home() {
   return (
@@ -37,23 +38,44 @@ function Home() {
               items-center
               gap-16
             `}>
-              <div>
+              <div className={`
+                flex
+                flex-col
+                items-center
+                justify-center
+              `}>
                 <h1 className={`
+                  relative
                   mb-4
+                  w-fit
                   text-balance
-                  text-5xl
+                  text-6xl
                   font-semibold
                   leading-tight
+                  tracking-tight
                 `}
                 >
-                  Hej der! 👋
+                  <span className={`
+                    absolute
+                    bottom-0
+                    left-0
+                    -translate-x-full
+                    select-none
+                    text-3xl
+                  `}
+                  >
+                    <Terminal />
+                  </span>
+                  Hej der!
                 </h1>
 
                 <p className={`
                   text-balance
                   text-2xl
                   leading-normal
-                `}>
+                  tracking-tight
+                `}
+                >
                   Jeg er en passioneret introvert, der godt kan lide at skabe
                   ting med kode
                 </p>

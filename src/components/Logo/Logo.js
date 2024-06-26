92baaad5d215baba7a55209c1b23145352912f3d
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-26 23:18:05 +0200

    Add Logo component
    - Created new Logo component in Logo/index.js and Logo/Logo.js.
    - The component includes an uppercase, bold link to the homepage styled with flex utilities. It also features a small circle styled with cyan color.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+import { Circle } from "react-feather";
+
+function Logo() {
+  return (
+    <Link className={`
+      flex
+      flex-col
+      font-bold
+      uppercase
+      leading-none
+    `} href="/">
+      <p>
+        <span className={`
+          inline-flex
+          w-full
+          items-baseline
+          justify-between
+        `}>
+          mads
+          <span className="inline-flex">
+            <Circle className={`
+              inline-block
+              h-1
+              w-1
+              fill-cyan-400
+              text-cyan-400
+            `}
+            />
+          </span>
+        </span>
+
+        <br />
+        codes
+      </p>
+    </Link>
+  );
+}
+
+export default Logo;

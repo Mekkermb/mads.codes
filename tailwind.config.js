42107efa591ff97957583c862eb44b56ebd91c31
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-27 16:10:38 +0200

    Update CSS styles and tailwind theme configuration
    - Removed color assignment for h1, h2, h3 in CSS
    - Added new color and background variables to the CSS root
    - Updated Tailwind theme colors to use the new CSS variables
    - Removed temporary safelist from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,40 @@
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-
-  safelist: ["bg-lime-200", "bg-amber-200"], // TODO Temp fix for JIT mode
   theme: {
+    colors: {
+      text: {
+        50: "hsl(var(--text-50))",
+        100: "hsl(var(--text-100))",
+        200: "hsl(var(--text-200))",
+        300: "hsl(var(--text-300))",
+        400: "hsl(var(--text-400))",
+        500: "hsl(var(--text-500))",
+        600: "hsl(var(--text-600))",
+        700: "hsl(var(--text-700))",
+        DEFAULT: "hsl(var(--text-800))",
+        900: "hsl(var(--text-900))",
+        950: "hsl(var(--text-950))",
+      },
+      background: {
+        DEFAULT: "hsl(var(--background-0))",
+        50: "hsl(var(--background-50))",
+        100: "hsl(var(--background-100))",
+        200: "hsl(var(--background-200))",
+        300: "hsl(var(--background-300))",
+        400: "hsl(var(--background-400))",
+        500: "hsl(var(--background-500))",
+        600: "hsl(var(--background-600))",
+        700: "hsl(var(--background-700))",
+        800: "hsl(var(--background-800))",
+        900: "hsl(var(--background-900))",
+        950: "hsl(var(--background-950))",
+      },
+      primary: "hsl(var(--primary))",
+      secondary: "hsl(var(--secondary))",
+      accent: "hsl(var(--accent))",
+    },
+
     extend: {
       fontFamily: {
         sans: ["var(--font-nunito-sans)"],

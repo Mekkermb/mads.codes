02e02aeef7dc615a53349a9d494dace4307f4c92
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-06-27 16:18:22 +0200

    Update CSS and Tailwind config with color shades
    - Add shades to primary, secondary, and accent colors in both styles.css and tailwind.config.js
    - Define new default colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         900: "hsl(var(--text-900))",
         950: "hsl(var(--text-950))",
       },
+
       background: {
         DEFAULT: "hsl(var(--background-0))",
         50: "hsl(var(--background-50))",
@@ -31,9 +32,48 @@ module.exports = {
         900: "hsl(var(--background-900))",
         950: "hsl(var(--background-950))",
       },
-      primary: "hsl(var(--primary))",
-      secondary: "hsl(var(--secondary))",
-      accent: "hsl(var(--accent))",
+
+      primary: {
+        50: "hsl(var(--primary-50))",
+        100: "hsl(var(--primary-100))",
+        DEFAULT: "hsl(var(--primary-200))",
+        300: "hsl(var(--primary-300))",
+        400: "hsl(var(--primary-400))",
+        500: "hsl(var(--primary-500))",
+        600: "hsl(var(--primary-600))",
+        700: "hsl(var(--primary-700))",
+        800: "hsl(var(--primary-800))",
+        900: "hsl(var(--primary-900))",
+        950: "hsl(var(--primary-950))",
+      },
+
+      secondary: {
+        50: "hsl(var(--secondary-50))",
+        100: "hsl(var(--secondary-100))",
+        DEFAULT: "hsl(var(--secondary-200))",
+        300: "hsl(var(--secondary-300))",
+        400: "hsl(var(--secondary-400))",
+        500: "hsl(var(--secondary-500))",
+        600: "hsl(var(--secondary-600))",
+        700: "hsl(var(--secondary-700))",
+        800: "hsl(var(--secondary-800))",
+        900: "hsl(var(--secondary-900))",
+        950: "hsl(var(--secondary-950))",
+      },
+
+      accent: {
+        50: "hsl(var(--accent-50))",
+        100: "hsl(var(--accent-100))",
+        200: "hsl(var(--accent-200))",
+        300: "hsl(var(--accent-300))",
+        400: "hsl(var(--accent-400))",
+        500: "hsl(var(--accent-500))",
+        600: "hsl(var(--accent-600))",
+        700: "hsl(var(--accent-700))",
+        DEFAULT: "hsl(var(--accent-800))",
+        900: "hsl(var(--accent-900))",
+        950: "hsl(var(--accent-950))",
+      },
     },
 
     extend: {
